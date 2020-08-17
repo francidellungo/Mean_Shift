585a23ecc87a1679cbe66037ed223fb28238c621
francesca <francidl96@gmail.com>
2020-08-17 21:41:48 +0200

    first commit

---
+++ MeanShift.java
@@ -0,0 +1,16 @@
+public class MeanShift{
+ 
+	public static void main(String args[]){
+
+		/*
+		Use System.out.println() to print on console.
+		*/
+		for(int i=0;i<10; i++){
+			System.out.println("Hello World ! " + i);
+			
+		}
+		
+
+	}
+
+	}
