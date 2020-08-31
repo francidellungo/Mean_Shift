e02202c49fe33b8c5e913bcc1f7d8a0d496646da
matpetrone <matteo.petrone@icloud.com>
2020-08-31 13:16:10 +0200

    generation of points

---
+++ cmake-build-debug/CMakeFiles/Mean_Shift.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mean_Shift.dir/genPoints.cpp.o"
+  "Mean_Shift"
+  "Mean_Shift.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mean_Shift.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
