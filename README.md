f84e188287b69219204d1b36eb045fb31aad32df
francesca <francidl96@gmail.com>
2020-10-01 12:44:22 +0200

    generate, save and plot dataset; readme updated

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
+# Mean Shift clustering
 [Mean shift](https://en.wikipedia.org/wiki/Mean_shift) clustering algorithm is an unsupervised clustering algorithm; in particular it is a non-parametric clustering  method based on kernel-density estimation.
 It aims to discover “blobs” in a smooth density of samples. It is a centroid-based algorithm, which works by updating candidates for centroids to be the mean of the points within a given region.
+
+
+![Alt Text](https://github.com/francidellungo/Mean_Shift/readme_imgs/mean-shift.gif)
+
+## Dataset
+Create new folder: `mkdir dataset` 
+
+
 
 ## WORK IN PROGRESS...
---
+++ generatePoints.py
@@ -0,0 +1,87 @@
+import sklearn.datasets as datasets
+import pandas as pd
+import numpy as np
+import os
+
+points = 100
+n_features = 3
+centers = 2
+std = 0.5
+datasets_dir = 'dataset'
+
+
+def generateData(n_points, n_features, n_clusters, std):
+    """
+    Generate dataset of n-dimensional points
+    :param n_points: total number of points
+    :param n_features: i-th point dimension (2 for x,y coordinates; 3 for x,y,z)
+    :param n_clusters: number of clusters to generate
+    :param std: standard deviation of the clusters
+    :return: data: generated data points, y: id of the cluster to which each element of the dataset belongs to
+    """
+    data, y = datasets.make_blobs(
+        n_points, n_features, centers=n_clusters, cluster_std=std, shuffle=True, random_state=1000)
+    return data, y
+
+
+def saveData(data, dest_dir):
+    """
+    Save dataset to file
+    :param data: dataset to be saved
+    :param dest_dir: directory where to save data
+    :return:
+    """
+    # save dataset to file
+    np.save(dest_dir, data)
+
+
+def loadData(source_dir):
+    """
+    Load dataset from file
+    :param source_dir: directory where to load data
+    :return: loaded dataset
+    """
+    d = np.load(source_dir + '.npy')
+    return d
+
+
+def plotData(data, c='blue'):
+    """
+    Plot 3d data
+    :param data: numpy array of data. dims: [n_points, 3]
+    :param c: colour (optional)
+    :return:
+    """
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+    # import numpy as np
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # select x, y, z values from data
+    xs = data[:, 0]
+    ys = data[:, 1]
+    zs = data[:, 2]
+
+    # plot data points
+    ax.scatter(xs, ys, zs, c=c)
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
+
+
+data, _ = generateData(points, n_features, centers, std)
+
+output_dir = os.path.join(datasets_dir, str(len(data)))
+
+saveData(data, output_dir)
+
+cc = loadData(output_dir)
+print(len(cc))
+
+plotData(cc)
+
